087471b42d547496d8e06a516a18f8df8f34f1fd
alissondiane <diane_10_96@hotmail.com>
2018-04-11 16:15:18 -0500

    imprimir

--- src/Componentes/Imprimir.js
+++ src/Componentes/Imprimir.js
@@ -13,8 +13,7 @@ class Imprimir extends React.Component {
     return (
       <div>
         <button  onClick={() => window.print()} className=" waves-effect waves-light btn imprimir ">Imprimir<i className="large material-icons left">local_printshop</i></button>
-
-        <p>Click above button opens print preview with these words on page</p>
+      
       </div>
 
     )
